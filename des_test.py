95c9d0d155d56a4903f4a6f2601cbae959a20ff9
Maxim <maxim.k.elasnce@gmail.com>
2024-10-11 23:20:58 +0300

    some shit

--- des_test.py
+++ des_test.py
@@ -2,7 +2,18 @@
 
 
 class DES:
+    def permutationStart(self, block64):
+        pass
+    def round(self, block64, roundKey):
+        pass 
     def encrypt(self, plain_text):
+        plainBytes = bytearray(plain_text, "utf-8")
+        while len(plainBytes) % 4 != 0: # Fill until divideble by 64 bits (8 * 4)
+            plainBytes.append(0x0)
+
+        plainBytes64 = []
+        for i in range(0, len(plainBytes), 4):
+            plainBytes64.append()
         pass
     def decrypt(self, cypher_text):
         pass
